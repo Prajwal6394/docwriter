ca1e5e03f9717376ecae56d08079762b311e9123
prajwal6394 <prajwalpandey2009@gmail.com>
2022-05-17 02:15:49 +0530

    Modal Component Build

--- src/Components/Docs.js
+++ src/Components/Docs.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Modal from './Modal';
 
-const Docs = () => {
-  return (
-    <div className='doc-container'>
-        <h1>Doc-Writer</h1>
+export default function Docs() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    return (
+        <div className='docs-main'>
+            <h1>Docs Clone</h1>
 
-        <button className='doc-button'>Add a Document</button>
-    </div>
-  )
+            <button
+                className='add-docs'
+                onClick={handleOpen}
+            >
+                Add a Document
+            </button>
+
+            <Modal
+                open={open}
+                setOpen={setOpen}
+            />
+        </div>
+    )
 }
-
-export default Docs
