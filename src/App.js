a72fa8731c2cc4c824f18a4c36ca3adf9ace192a
prajwal6394 <prajwalpandey2009@gmail.com>
2022-05-17 01:57:23 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Docs from './Components/Docs';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Docs/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Docs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Docs = () => {
+  return (
+    <div className='doc-container'>
+        <h1>Google Docs Clone</h1>
+    </div>
+  )
+}
+
+export default Docs
